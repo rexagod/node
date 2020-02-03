c46c6e806374f34a1deea6ec3565549b6f6e5e9e
rexagod <rexagod@gmail.com>
2020-02-03 18:48:25 +0530

    _

---
+++ test/parallel/test-readline-input-stream-error-event.js
@@ -0,0 +1,30 @@
+'use strict';
+require('../common');
+
+// This test ensures that the "readline" module correctly
+// emits an error on the `options.input` stream whenever
+// it encounters an error (a fake stream created by a non
+// existent file in this case).
+
+const assert = require('assert');
+const {
+  ERR_STREAM_PREMATURE_CLOSE
+} = require('../../lib/internal/errors').codes;
+const fs = require('fs');
+const readline = require('readline');
+
+function emit_error_on_stream(filename) {
+  return function() {
+    const fileStream = fs.createReadStream(filename);
+    readline.createInterface({
+      input: fileStream,
+      crlfDelay: Infinity
+    });
+  };
+}
+
+assert.throws(
+  emit_error_on_stream('foobar.txt'),
+  ERR_STREAM_PREMATURE_CLOSE,
+  'ERR_STREAM_PREMATURE_CLOSE'
+);
